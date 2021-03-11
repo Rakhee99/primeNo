621494a4c388d3f6a3d09eca29fbe67981f45a56
Rakhee99 <80023884+Rakhee99@users.noreply.github.com>
2021-03-11 13:04:59 +0530

    commiting prime

---
+++ src/main/java/com/example/pnum/PnumApplication.java
@@ -0,0 +1,13 @@
+package com.example.pnum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pnum/PnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pnum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
